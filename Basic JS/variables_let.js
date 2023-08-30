0a6e89652447667aee13124a3ddef68229d9be49
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-30 11:42:14 -0300

    variaveis com let

---
+++ Basic JS/variables_let.js
@@ -0,0 +1,9 @@
+let nome = 'joao';
+let nome2 = 'maria';
+let testeUndefined;
+console.log(testeUndefined);
+console.log(nome, 'tem 35 anos');
+console.log(nome, 'conheceu', nome2);
+console.log(nome, 'casou-se com', nome2);
+testeUndefined = "defined";
+console.log(testeUndefined);
