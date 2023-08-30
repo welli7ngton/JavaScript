a34685311b761eb53b2b175b46e96abf66bb0ca0
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-30 11:41:48 -0300

    exercicio com variaveis

---
+++ Basic JS/variables_exercice.js
@@ -0,0 +1,15 @@
+const myName = 'Otavio Miranda';
+let age = 30;
+let height = 1.80;
+let weight = 84;
+const birth = 2000;
+let imc; // peso / (altura * altura)
+
+imc = weight / (height * height);
+
+console.log(myName, 'tem', age, 'anos', ',pesa',weight, 'kgs, tem', height, 'm de altura e seu IMC é:', imc);
+
+console.log(myName,'nasceu em', birth);
+
+console.log();
+console.log(`${myName} tem ${age} anos, pesa ${weight} kgs, tem ${height} m de altura e seu IMC é: ${imc}`)
