8139f2d0d4c193a3c8e487411b3680613effce96
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-30 11:40:25 -0300

    tipos de dados primitivos do JavaScript

---
+++ Basic JS/js_primitive_datas.js
@@ -0,0 +1,19 @@
+const name1 = 'wellington';  // string
+const name2 = "Ameida";  // string
+const name3 = `Silva`;  // string
+
+const num1 = 10;  // number
+const num2 = 54.324;  // number
+
+let studentName;  // undefined
+const studentLastName = null  // null value
+
+let boolean1 = true;  // true | false
+let boolean2 = false;  // true | false
+
+console.log(typeof(name1, name2, name3));
+
+console.log(typeof(num1, num2));
+console.log(typeof(studentName, studentLastName));
+console.log(typeof boolean1);
+console.log(typeof boolean2);
