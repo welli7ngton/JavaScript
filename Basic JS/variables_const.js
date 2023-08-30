588b4002aaa96fc31f02aa91a549437efbe3b593
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-30 11:41:03 -0300

    variaveis constantes

---
+++ Basic JS/variables_const.js
@@ -0,0 +1,22 @@
+const name = 'wellington';
+console.log(name);
+
+// erro de inialização, constantes não podem ser inicializadas sem valor
+// const testNotInitialized;
+
+const firstNumber = '5'; // string
+const secondNumber = 10; // number
+
+const result = firstNumber + secondNumber;
+console.log('result 1');
+console.log(result);
+
+let result2 = 300;
+
+result2 = result2 + 20;
+console.log('result 2');
+console.log(result2);
+console.log(result2 -20);
+
+let foo = firstNumber + secondNumber
+console.log(typeof(foo))
