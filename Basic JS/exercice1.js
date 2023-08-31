2c0b4528c90ecf65bb64f7faa36d05eae3a79f11
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-31 10:33:15 -0300

    exercicio usando variaveis

---
+++ Basic JS/exercice1.js
@@ -0,0 +1,13 @@
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+// let temp = varC;
+
+// varC = varA;
+// varA = varB;
+// varB = temp;
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
