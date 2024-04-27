314f1262123ddfc5c25194ee5af30f9b5922bda5
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-27 12:14:47 -0300

    for each

---
+++ Arrays/for_each.js
@@ -0,0 +1,7 @@
+const numeros = [4,24,54,2,36,8,4,3,3,69,2,134,6,4,7,]
+
+numeros.forEach(function(valor, indice, array){
+    console.log(valor)
+    console.log(indice)
+    console.log(array)
+})
