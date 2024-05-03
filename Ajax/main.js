4983f4f9e1953098d23fe8e570f37fb463fc011c
welli7ngton <wellingtonasilva45@gmail.com>
2024-05-03 13:26:39 -0300

    js code e paginas para testes

---
+++ Ajax/main.js
@@ -0,0 +1,47 @@
+// AJAX - Asynchronous JavaScript And XML
+
+const req = obj =>{
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open(obj.method, obj.url, true)
+        xhr.send()
+    
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 200 && xhr.status < 300){
+                resolve(xhr.responseText)
+            } else {
+                reject(xhr.statusText)
+            }
+        })
+    })
+}
+
+document.addEventListener('click', e =>{
+    const el = e.target
+    const tag = el.tagName.toLowerCase()
+
+    if (tag === 'a'){
+        e.preventDefault()
+        carregaPagina(el)
+    }
+})
+
+
+async function carregaPagina(el){
+    const href = el.getAttribute('href')
+    
+    try {
+        const response = await req({
+            method: 'GET',
+            url: href,
+        })
+        carregaResutado(response)
+    } catch(err){
+        console.log(err)
+    }
+}
+
+function carregaResutado(response){
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = response
+}
